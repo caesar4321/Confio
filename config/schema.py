5fdb7265c87e60ff7b6a4e6d72a733c85991bd87
Julian <julian@koolpeace.eu>
2023-11-22 22:25:09 -0600

    schema

--- config/schema.py
+++ config/schema.py
@@ -1,13 +1,3 @@
 import graphene
-from graphene_django.types import DjangoObjectType
 
-class Query(DjangoObjectType):
-	pass
-
-class Mutation(DjangoObjectType):
-	pass
-
-class Subscription(DjangoObjectType):
-	pass
-
-schema = graphene.Schema(query=Query, mutation=Mutation, subscription=Subscription)
+schema = graphene.Schema()
