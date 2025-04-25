b25b83187e5eff8d44e686a218e0d0324a0d1059
caesar4321 <julian@koolpeace.eu>
2025-04-24 20:59:09 -0600

    Update Android configuration and add keystore to git-crypt

---
+++ apps/react-native.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  project: {
+    android: {
+      sourceDir: './android',
+      packageName: 'com.Confio.Confio',
+    },
+    ios: {
+      sourceDir: './ios',
+    },
+  },
+  dependencies: {
+    '@react-native-firebase/app': {},
+    '@react-native-firebase/auth': {},
+    '@react-native-google-signin/google-signin': {},
+    'react-native-get-random-values': {},
+    'react-native-keychain': {},
+    'react-native-vector-icons': {},
+    'react-native-svg': {},
+  },
+}; 
